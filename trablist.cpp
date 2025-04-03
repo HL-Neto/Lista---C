e67af28af263caf913fdefc79085b4b546aa02c3
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-04-03 20:22:08 -0300

    trablist.cpp 1

--- trablist.cpp
+++ trablist.cpp
@@ -1,12 +1,11 @@
-#include	<stdio.h>
-#include	<string.h>
-#include	<locale.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
 
-#define MAX 100
+#define MAX 60
 
-// DefiniÁ„o b·sica dos elementos na lista
 typedef struct {
-    char nome[50];
+    int rgm;
 } Aluno;
 
 typedef struct {
@@ -14,14 +13,12 @@ typedef struct {
     int ultimaPosicao;
 } Lista;
 
-// Criar lista vazia
 Lista criar() {
     Lista lista;
     lista.ultimaPosicao = -1;
     return lista;
 }
 
-// VerificaÁıes b·sicas da lista
 int listaVazia(Lista *lista) {
     return (lista->ultimaPosicao == -1);
 }
@@ -34,55 +31,91 @@ int tamanhoAtualLista(Lista *lista) {
     return lista->ultimaPosicao + 1;
 }
 
+int buscaSequencial(Lista *lista, int rgm_busca) {
+    int i = 0;
+    while (i <= lista->ultimaPosicao) {
+        if (lista->vetor[i].rgm == rgm_busca) {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
 
+int removerContatoPorNome(Lista *lista, int rgm) {
+    int indice = buscaSequencial(lista, rgm);
+    if (indice != -1) {
+        for (int i = indice; i < lista->ultimaPosicao; i++) {
+            lista->vetor[i] = lista->vetor[i + 1];
+        }
+        lista->ultimaPosicao--;
+        printf("RGM excluido\n");
+        return 0;
+    } else {
+        return -1;
+    }
+}
 
-int main(){
-	int resposta;
-	Lista novaLista = criar();
-do{
+int main() {
+    int resposta;
+    Lista novaLista = criar();
 
-	printf("Menu\n 1.Criar Aluno\n 2.Listar alubos\n 3.Pesquisar aluno\n 4.Excluir aluno\n 5.Sair\n");
-	printf("escolha uma opcao:\n");
-	scanf("%d", &resposta);
-	getchar();
-	
-	switch(resposta){
-		case 1:
-			if (listaCheia(&novaLista)) {
-                        printf("A lista est· cheia e n„o d· para adicionar novos contatos!\n");
+    do {
+        printf("Menu\n 1.INCLUIR\n 2.BUSCAR\n 3.MOSTRAR\n 4.REMOVER \n");
+        printf("escolha uma opcao:\n");
+        scanf("%d", &resposta);
+
+        switch (resposta) {
+            case 1:
+                if (listaCheia(&novaLista)) {
+                    printf("A lista est√° cheia e n√£o d√° para adicionar novos contatos!\n");
+                } else {
+                    Aluno novo_rgm; 
+                    printf("Qual o rgm que voc√™ deseja adicionar?:\n");
+                    scanf("%d", &novo_rgm.rgm);
+                    novaLista.vetor[++novaLista.ultimaPosicao] = novo_rgm;
+                    printf("Aluno adicionado\n");
+                }
+                break;
+
+            case 2:
+                if (listaVazia(&novaLista)) {
+                    printf("A lista est√° vazia.\n");
+                } else {
+                    int rgm_busca;
+                    printf("Digite o rgm:\n");
+                    scanf("%d", &rgm_busca);
+                    int busca = buscaSequencial(&novaLista, rgm_busca);
+                    if (busca == -1) {
+                        printf("n√£o existe;\n");
                     } else {
-                        Aluno novoAluno;
-                        printf("Qual o nome do contato que vocÍ deseja adicionar?:\n");
-                        fgets(novoAluno.nome, 50, stdin);
-                        novoAluno.nome[strcspn(novoAluno.nome, "\n")] = '\0';
-
-                        novaLista.vetor[++novaLista.ultimaPosicao] = novoAluno;
-                        printf("Aluno adicionado!\n");
+                        printf("matricula %d rgm %d\n", busca, rgm_busca);
                     }
-                    break;
-		
-		case 2:
-			if (listaVazia(&novaLista)) {
-                        printf("A lista est· vazia.\n");
-                    } else {
-                        for (int i = 0; i <= novaLista.ultimaPosicao; i++) {
-                            printf("%d. %s\n", i + 1, novaLista.vetor[i].nome);
-                        }
-                    }
-                    break;
-		
-		case 3:
-			printf("Pesquisar aluno");
-			break;
-		
-		case 4:
-			printf("Excluir aluno");
-			break;
-		
-		case 5:
-			printf("Sair");
-			break;
-	}
-} while (resposta != 5);
-}		
+                }
+                break;
 
+            case 3:
+                if (listaVazia(&novaLista)) {
+                    printf("A lista est√° vazia.\n");
+                } else {
+                    for (int i = 0; i <= novaLista.ultimaPosicao; i++) {
+                        printf("matricula : %d rgm : %d\n", i + 1, novaLista.vetor[i].rgm);
+                    }
+                }
+                break;
+
+            case 4:
+                printf("Qual o rgm?:\n");
+                int rgm; 
+                scanf("%d", &rgm);
+                removerContatoPorNome(&novaLista, rgm);
+                break;
+
+            case 5:
+                printf("Sair\n");
+                break;
+        }
+    } while (resposta != 5);
+
+    return 0;
+}
